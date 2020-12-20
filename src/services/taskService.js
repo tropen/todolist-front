789ea4cddc4479ae43a9bb7636a1cd4e78b0934e
tropen <tropen09@gmail.com>
2020-12-20 14:52:33 +0200

    codestyle

--- src/services/taskService.js
+++ src/services/taskService.js
@@ -7,6 +7,4 @@ export const getTaskStatuses = () => api.get(baseApi + `/statuses`)
 export const getTask = id => api.get(`${baseApi}/${id}`)
 export const addTask = data => api.post(baseApi, data)
 export const updateTask = (taskId, data) => api.put(`${baseApi}/${taskId}`, data)
-
 export const deleteTask = id => api.delete(`${baseApi}/${id}`)
-
